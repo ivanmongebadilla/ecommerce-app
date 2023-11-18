f4b2cab879c4135f7764caa1fee52997e488e27a
ivanmongebadilla <65142724+ivanmongebadilla@users.noreply.github.com>
2023-11-18 09:50:49 -0700

    Update Header Link

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,15 @@ import { Fragment } from 'react';
 import '../../style/style.css';
 import HeaderImg from '../../assets/open-source-ecommerce.jpg';
 import HeaderCartButton from './HeaderCartButton';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className='header'>
-                <h1>ReactECommerce</h1>
+                <Link to="/">
+                    <h1>ReactECommerce</h1>
+                </Link>
                 <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className='main-image'>
